7e519a1f7c914bf8d34b9ff010f51d1bc8570884
Suman S <147817337+sumans-19@users.noreply.github.com>
2025-03-05 09:38:06 +0530

    Update lab02(merge sort).java

--- lab02(merge sort).java
+++ lab02(merge sort).java
@@ -4,63 +4,88 @@ of n, the number of elements in the list to be sorted and plot a graph of the ti
 taken versus n. The elements can be read from a file or can be generated using the 
 random number generator.
 
+package DAALab;
 
+import java.util.*;
 
-import java.util.Random;
-import java.util.Scanner;
+public class mergesort {
 
-public class Mergesort {
 	static final int MAX = 10005;
 	static int[] a = new int[MAX];
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter Max array size: ");
+		System.out.println("enter the numbe rof elemnts required : ");
 		int n = input.nextInt();
-		Random random = new Random();
-		System.out.println("Enter the array elements: ");
-		for (int i = 0; i < n; i++)
-			a[i] = random.nextInt(1000);
-		for (int i = 0; i < n; i++)
-			System.out.print(a[i] + " ");
+		Random random = new Random(1000);
+		System.out.println("enter your choice :  1.Enter the elements manually 2.Randomly generate the value");
+		int choice = input.nextInt();
+		switch (choice) {
+		case 2:
+			for (int i = 0; i < n; i++) {
+				a[i] = random.nextInt(1000);
+			}
+			break;
+		case 1: 
+			for (int i = 0; i < n; i++) {
+				a[i] = input.nextInt();
+			}
+			break;
+		default:
+			System.out.println("Invalid choice");
+		}
+		System.out.println("the Array elemnts are : ");
+		for (int i = 0; i < n; i++) {
+			System.out.println(a[i] + " ");
+		}
+		
 		long startTime = System.nanoTime();
-		MergeSortAlgo(0, n - 1);
-		long stopTime = System.nanoTime();
-		long elapsedTime = stopTime - startTime;
-		System.out.println("Time Complexity (ms) for n = " + n + " is : " + (double) elapsedTime / 1000000);
-		System.out.println("Sorted Array (Merge Sort):");
-		for (int i = 0; i < n; i++)
-			System.out.print(a[i] + " ");
-		input.close();
-	}
-
-	public static void MergeSortAlgo(int low, int high) {
-		int mid;
-		if (low < high) {
-			mid = (low + high) / 2;
-			MergeSortAlgo(low, mid);
-			MergeSortAlgo(mid + 1, high);
-			Merge(low, mid, high);
+		MergeSort(0,n-1);
+		long endTime = System.nanoTime();
+		long timeElapsed = endTime - startTime;
+		System.out.println("time complexity for n = " + n + " in ms is " +
+				(double)timeElapsed/100000);
+		
+		System.out.println("the sorted elemnts are :");
+		for(int i=0;i<n;i++) {
+			System.out.println(a[i] + " ");
 		}
 	}
-
-	public static void Merge(int low, int mid, int high) {
-		int[] b = new int[MAX];
-		int i, h, j, k;
-		h = i = low;
-		j = mid + 1;
-		while ((h <= mid) && (j <= high))
-			if (a[h] < a[j])
-				b[i++] = a[h++];
-			else
-				b[i++] = a[j++];
-		if (h > mid)
-			for (k = j; k <= high; k++)
-				b[i++] = a[k];
-		else
-			for (k = h; k <= mid; k++)
-				b[i++] = a[k];
-		for (k = low; k <= high; k++)
-			a[k] = b[k];
+	
+	public static void MergeSort(int low , int high) {
+		int mid;
+		if(low<high) {
+			mid = (low+high) / 2;
+			MergeSort(low,mid);
+			MergeSort(mid+1,high);
+			Merge(low,mid,high);
+		}
+		
 	}
-}
+	
+	public static void Merge(int low, int mid , int high) {
+		int[] b = new int[MAX];
+		int i,j,k,h;
+		h=i=low;
+		j=mid+1;
+		while((h<= mid ) && (j<=high)) {
+			if(a[h] < a[j]) {
+				b[i++] = a[h++];}
+				else {
+					b[i++] = a[j++];
+				}
+		if(h>mid){
+			for(k=j;k<=mid ;k++) {
+				b[i++] = a[k];
+			}
+		}else {
+			for(k=h;k<=mid ;k++) {
+				b[i++] = a[k];			}
+		}
+		for(k = low ;k<=mid ;k++) {
+			a[k] = b[k];
+		}
+		}
+				
+			}
+	}
